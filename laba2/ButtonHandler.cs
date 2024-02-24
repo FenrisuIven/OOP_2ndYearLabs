6b8bb43a4c4ce898d6b074962737b3b09df4df07
FenrisuIven <120093556+FenrisuIven@users.noreply.github.com>
2024-02-24 21:09:22 +0200

    Changed button handling and exp evaluation

---
+++ laba2/ButtonHandler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace laba2
+{
+    public partial class MainWindow
+    {
+        private void AddToList(string val)
+        {
+            input.Add(val);
+            DisplayInput();
+        }
+
+        public void ButtonExec(string name)
+        {
+            AddToList(name);
+            ReformatInputList();
+            DisplayRes();
+        }
+
+#region Digit Buttons
+        private void btn1_Click(object sender, RoutedEventArgs e) => AddToList("1");
+        private void btn2_Click(object sender, RoutedEventArgs e) => AddToList("2");
+        private void btn3_Click(object sender, RoutedEventArgs e) => AddToList("3");
+        private void btn4_Click(object sender, RoutedEventArgs e) => AddToList("4");
+        private void btn5_Click(object sender, RoutedEventArgs e) => AddToList("5");
+        private void btn6_Click(object sender, RoutedEventArgs e) => AddToList("6");
+        private void btn7_Click(object sender, RoutedEventArgs e) => AddToList("7");
+        private void btn8_Click(object sender, RoutedEventArgs e) => AddToList("8");
+        private void btn9_Click(object sender, RoutedEventArgs e) => AddToList("9");
+        private void btn0_Click(object sender, RoutedEventArgs e) => AddToList("0");
+        private void btn00_Click(object sender, RoutedEventArgs e) => AddToList("00");
+        private void btnDot_Click(object sender, RoutedEventArgs e) => AddToList(".");
+
+#endregion
+
+#region Operations
+        private void btnPlus_Click(object sender, RoutedEventArgs e) => ButtonExec("+");
+        private void btnMinus_Click(object sender, RoutedEventArgs e) => ButtonExec("-");
+        private void btnMultiply_Click(object sender, RoutedEventArgs e) => ButtonExec("×");
+        private void btnDide_Click(object sender, RoutedEventArgs e) => ButtonExec("÷");
+
+        private void btnEqual_Click(object sender, RoutedEventArgs e)
+        {
+            ReformatInputList();
+            DisplayRes();
+            txtInput.Foreground = Brushes.LightGray;
+            input = new List<string>();
+        }
+
+
+        private void btnC_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Text = "";
+            input = new List<string>();
+            txtOutput.Text = "";
+        }
+#endregion
+
+    }
+}
--- laba2/MainWindow.xaml.cs
+++ laba2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,7 +10,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using org.mariuszgromada.math.mxparser;
+using Expression = org.mariuszgromada.math.mxparser.Expression;
 
 namespace laba2
 {
@@ -18,127 +20,27 @@ namespace laba2
     //пока гружу просто рабочую версию, почищу уже потом (тут много ненужного скорее всего)
     public partial class MainWindow : Window
     {
-        List<string> input = new List<string>();
-        List<string> latestNum = new List<string>();
-        double res = 0;
-        bool firstNum = true;
+        private List<string> input = new List<string>();
 
-        bool rememberNum = false;
-        char prevAction = new char();
-        public MainWindow()
+        public MainWindow() { InitializeComponent(); }
+
+        private void DisplayInput() 
         {
-            InitializeComponent();
-        }
-        private void DisplayInput()
-        {
-            txtInput.Text = string.Join("", input);
-        }
-        private void AddToLists(string val)
-        {
-            input.Add(val);
-            if (rememberNum) latestNum.Add(val);
-            DisplayInput();
+            if (txtInput.Foreground != Brushes.Black) txtInput.Foreground = Brushes.Black;
+            txtInput.Text = string.Join("", input).Replace('.', ','); 
         }
 
-#region Digit Buttons
-        private void btn1_Click(object sender, RoutedEventArgs e) => AddToLists("1");
-        private void btn2_Click(object sender, RoutedEventArgs e) => AddToLists("2");
-        private void btn3_Click(object sender, RoutedEventArgs e) => AddToLists("3");
-        private void btn4_Click(object sender, RoutedEventArgs e) => AddToLists("4");
-        private void btn5_Click(object sender, RoutedEventArgs e) => AddToLists("5");
-        private void btn6_Click(object sender, RoutedEventArgs e) => AddToLists("6");
-        private void btn7_Click(object sender, RoutedEventArgs e) => AddToLists("7");
-        private void btn8_Click(object sender, RoutedEventArgs e) => AddToLists("8");
-        private void btn9_Click(object sender, RoutedEventArgs e) => AddToLists("9");
-        private void btn0_Click(object sender, RoutedEventArgs e) => AddToLists("0");
-        private void btn00_Click(object sender, RoutedEventArgs e) => AddToLists("00");
-#endregion
-
-#region Operations
-        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        public void DisplayRes()
         {
-            ReformatInputList();
-            AddToLists("+");
-            rememberNum = true;
-            OpButtonAction('+');
-        }
-
-
-        private void btnMinus_Click(object sender, RoutedEventArgs e)
-        {
-            ReformatInputList();
-            AddToLists("-");
-            rememberNum = true;
-            OpButtonAction('-');
-        }
-
-        private void btnMultiply_Click(object sender, RoutedEventArgs e)
-        {
-            ReformatInputList();
-            AddToLists("×");
-            rememberNum = true;
-            OpButtonAction('×');
-        }
-
-        private void btnDide_Click(object sender, RoutedEventArgs e)
-        {
-            ReformatInputList();
-            AddToLists("÷");
-            rememberNum = true;
-            OpButtonAction('÷');
-        }
-#endregion
-
-        private void btnEqual_Click(object sender, RoutedEventArgs e)
-        {
-            OpButtonAction('=');
-            prevAction = new char();
-            txtInput.Text = " ";
-            input = new List<string>();
-            rememberNum = false;
-        }
-
-        public void OpButtonAction(char op)
-        {
-            if (prevAction == '\0') 
-            { 
-                prevAction = op;
-                res = double.Parse(input.ElementAt(0));
-                txtOutput.Text = $"{res}";
-                return;
-            }
-            if (op != '=') latestNum.RemoveAt(latestNum.Count - 1);
-            string lastNum = latestNum.Count != 1 ? string.Join("", latestNum) : latestNum.ElementAt(0);
-            latestNum = new List<string>();
-
-            switch (prevAction)
-            {
-                case '+':
-                    res += double.Parse(lastNum);
-                    break;
-
-                case '-':
-                    res -= double.Parse(lastNum);
-                    break;
-
-                case '×':
-                    res *= double.Parse(lastNum);
-                    break;
-
-                case '÷':
-                    res /= double.Parse(lastNum);
-                    break;
-            }
-            prevAction = Convert.ToChar(input.ElementAt(input.Count - 1));
-            txtOutput.Text = $"{res}";
-            ReformatInputList();
+            Expression ex = new Expression(input.ElementAt(0));
+            txtOutput.Text = $"{ex.calculate()}";
         }
 
         public void ReformatInputList ()
         {
             string temp = string.Join("", input);
             input = new List<string> { temp };
-            if (firstNum) { res += double.Parse(temp); firstNum = false; }
         }
+
     }
 }
